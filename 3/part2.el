0090c734696e0c405d3f019a29c94a24cc13d294
Pat McGee <patmcgee331@gmail.com>
2021-12-04 00:48:44 -0500

    day 3 revisit

--- 3/part2.el
+++ 3/part2.el
@@ -2,8 +2,7 @@
   (let ((lines (length report-lines))
         (sum (seq-reduce
               (lambda (acc line)
-                (progn
-                   (+ (string-to-number (nth current-bit line)) acc)))
+                (+ (string-to-number (nth current-bit line)) acc))
               report-lines
               0)))
     (if (< sum (- lines sum)) "0" "1")))
@@ -18,18 +17,26 @@
   (if (eq (length report-lines) 1)
       (string-to-number (mapconcat 'identity (car report-lines) "") 2)
     (aoc-find-rating
-     (seq-filter (apply-partially filter-func (aoc-calc-most-common-bit report-lines current-bit) current-bit) report-lines)
+     (seq-filter
+      (apply-partially
+       filter-func
+       (aoc-calc-most-common-bit report-lines current-bit)
+       current-bit)
+      report-lines)
      (+ 1 current-bit)
      filter-func)))
+
+(defun aoc-parse-report ()
+  (seq-filter
+   (lambda (line) (not (equal line nil)))
+   (mapcar
+    (lambda (line) (seq-filter (lambda (char) (not (equal char ""))) (split-string line "")))
+    (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n"))))
 
 (defun aoc-calc-life-support-ratings ()
   "Calculate oxygen generator and CO2 scurbber ratings using diagnostic report"
   (interactive)
-  (let ((report-lines (seq-filter
-                       (lambda (line) (not (equal line nil)))
-                       (mapcar
-                        (lambda (line) (seq-filter (lambda (char) (not (equal char ""))) (split-string line "")))
-                        (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n")))))
+  (let ((report-lines (aoc-parse-report)))
     (message
      "%s"
      (*
