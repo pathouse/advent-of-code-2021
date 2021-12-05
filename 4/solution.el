2348ae04b2ec0d77b28f2dc8c41be8a5ec42d89e
Pat McGee <patmcgee331@gmail.com>
2021-12-04 21:31:01 -0500

    day 4

--- 4/solution.el
+++ 4/solution.el
@@ -32,8 +32,11 @@
 (defun aoc-check-row (row)
   (seq-every-p (lambda (item) (car (cdr item))) row))
 
-(defun aoc-flatten-nums (lists)
-  (mapcan (lambda (row) (mapcan (lambda (item) (car item)) row)) lists))
+(defun aoc-flatten-one (predicate list)
+  (mapcar (lambda (item) (if (eq (car (cdr item)) predicate) (car item) 0)) list))
+
+(defun aoc-flatten-many (predicate lists)
+  (mapcan (apply-partially 'aoc-flatten-one predicate) lists))
 
 (defun aoc-init-state (lists)
   (mapcar (lambda (list) (mapcar (lambda (element) (list element nil)) list)) lists))
@@ -43,28 +46,47 @@
                                    (list (car element) (or (car (cdr element))
                                                            (eq (car element) num)))) l)) lists))
 
+(defun aoc-bingo-score (rows cols winning-seq)
+  (let ((all-nums (aoc-flatten-many nil (append rows cols)))
+        (winning-nums (aoc-flatten-one t winning-seq)))
+    (message "all: %s" all-nums)
+    (message "winning: %s" winning-nums)
+    (message "diff: %s" (seq-uniq (seq-difference all-nums winning-nums)))
+    (message "score: %s" (seq-reduce #'+ (seq-uniq (seq-difference all-nums winning-nums)) 0))
+    (seq-reduce #'+ (seq-uniq (seq-difference all-nums winning-nums)) 0)))
+
 (defun aoc-play-bingo (rows cols numbers turn)
   (let ((updated-rows (aoc-update-state rows (car numbers)))
         (updated-cols (aoc-update-state cols (car numbers))))
-    (message "number: %s\nrows: %s\ncols:%s\nturn:%s" (car numbers) updated-rows updated-cols turn)
     (let ((winning-seq (aoc-complete-seq (append updated-rows updated-cols))))
-      (if winning-seq
-          (list
-           turn
-           (car numbers)
-           (seq-reduce #'+ (seq-difference (aoc-flatten-nums (append updated-rows updated-cols)) winning-seq)))
-        (aoc-play-bingo updated-rows updated-cols (cdr numbers) (+ 1 turn))))))
+      (cond (winning-seq (list
+                          turn
+                          (car numbers)
+                          (aoc-bingo-score updated-rows updated-cols winning-seq)))
+            ((eq 0 (length (cdr numbers))) '(nil nil nil))
+            (t (aoc-play-bingo updated-rows updated-cols (cdr numbers) (+ 1 turn)))))))
 
 (defun aoc-calc-bingo-win-conditions (numbers turn board)
   (let ((rows (aoc-init-state board))
         (cols (aoc-init-state (aoc-get-board-cols board '(() () () () ())))))
     (aoc-play-bingo rows cols numbers turn)))
 
-(defun aoc-bingo ()
-  (interactive)
+(defun aoc-squid-bingo (sort-by buffer-name)
   (let ((numbers (aoc-get-bingo-numbers))
         (boards (aoc-get-bingo-boards))
-        (b (get-buffer-create "bingo-boards")))
+        (b (get-buffer-create buffer-name)))
     (select-window (split-window-vertically))
     (switch-to-buffer b)
-    (insert (format "%s" (mapcar (apply-partially 'aoc-calc-bingo-win-conditions numbers 0) boards)))))
+    (let ((winner (car (seq-sort-by #'car sort-by (mapcar (apply-partially 'aoc-calc-bingo-win-conditions numbers 0) boards)))))
+      (insert (format "%s: %d" buffer-name (* (nth 1 winner) (nth 2 winner)))))))
+
+;; it's always nice when the solution for part one ends up being just 1 small modification away from the solution for part 2 :)
+;; in this case deciding to solve part 1 by computing win conditions for all boards ended up paying off
+
+(defun aoc-squid-bingo-first ()
+  (interactive)
+  (aoc-squid-bingo #'< "first-winning-board"))
+
+(defun aoc-squid-bingo-last ()
+  (interactive)
+  (aoc-squid-bingo #'> "last-winning-board"))
